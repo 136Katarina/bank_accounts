d79ede0bc07beec4ddbee2df8ed2047b12b56752
annalanigan <annalanigan@outlook.com>
2018-01-23 14:36:02 +0000

    found account by name test passed

--- bank.js
+++ bank.js
@@ -10,4 +10,13 @@ Bank.prototype.addAccount = function(account){
   this.accounts.push(account);
 }
 
+
+Bank.prototype.findByName = function(name){
+  for (account of this.accounts) {
+    if (account.name === name){
+      return account;
+    }
+  }
+};
+
 module.exports = Bank;
--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -14,18 +14,26 @@ describe('bank', function(){
     bank = new Bank();
     account1 = new Account('Katarina',4000, 'Business');
     account2 = new Account('Anna',2000, 'Personal');
-    account2 = new Account('Jardine',100, 'Personal');
-    account2 = new Account('Evil Fraser',10000, 'Saving');
+    account3 = new Account('Jardine',100, 'Personal');
+    account4 = new Account('Evil Fraser',10000, 'Saving');
+    bank.addAccount(account1);
+    bank.addAccount(account2);
+    bank.addAccount(account3);
+    bank.addAccount(account4);
   })
 
+  //
+  // it('should nave no accounts to start with', function(){
+  //   assert.strictEqual(bank.accountCount(), 0);
+  // })
+  //
+  // it('should be able to add an account', function(){
+  //   bank.addAccount(account1)
+  //   assert.strictEqual(bank.accountCount(), 1);
+  // })
 
-  it('should nave no accounts to start with', function(){
-    assert.strictEqual(bank.accountCount(), 0);
-  })
-
-  it('should be able to add an account', function(){
-    bank.addAccount(account1)
-    assert.strictEqual(bank.accountCount(), 1);
+  it('should find account by name', function(){
+    assert.strictEqual(bank.findByName("Katarina"),account1);
   })
 
 
